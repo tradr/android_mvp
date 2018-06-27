dd05453d3d246f31db42fa96c395bc26c9495d95
svenji <shong@whistle.com>
2018-06-27 15:15:20 -0700

    Refactor boilerplate to use tradr package name

--- app/src/main/kotlin/com/sven/ui/main/viewmodel/MainViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.sven.ui.main.viewmodel
-
-import com.sven.mvvm.viewmodel.ViewModel
-import javax.inject.Inject
-
-class MainViewModel @Inject constructor(): ViewModel() {
-    override fun bind() {
-    }
-}
